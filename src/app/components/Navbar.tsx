7c4a4cd01af09627d2bbce3cfaa3218e1d0d050b
Mike Thomas <mikethomas84@gmail.com>
2024-02-24 19:26:37 +0000

    navbar comp init

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <nav className='sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
+        <MaxWidthWrapper>
+            <div className='flex h-14 items-center justify-between border-b border-zinc-200'>
+                <Link href='/' className="flex z-40 font-semibold">
+                    <span>PDFriend</span>
+                </Link> 
+            </div>
+        </MaxWidthWrapper>
+    </nav>
+  )
+}
+ 
+export default Navbar
