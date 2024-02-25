27e0531cdcd74b219a8ccb8b2150d0d5619521fa
Mike Thomas <mikethomas84@gmail.com>
2024-02-25 17:05:31 +0000

    search params

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,8 +1,13 @@
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
     const router = useRouter()
+
+    const searchParams = useSearchParams()
+    const origin = searchParams.get('origin')
+
+    
 }
 
 export default page
