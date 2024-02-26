dbf3d1873c4f6430bc25a55a62c81e0b223db864
Mike Thomas <mikethomas84@gmail.com>
2024-02-26 15:31:00 +0000

    trpc provider

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -1,6 +1,19 @@
 'use client'
 
+import { useState } from "react"
+import { QueryClient } from "@tanstack/react-query"
+import { trpc } from "../_trpc/client"
+import { httpBatchLink } from "@trpc/client"
+
 const Providers = () => {
+    const [queryClient] = useState(() => new QueryClient())
+    const [trpcClient] = useState(() => trpc.createClient({
+        links: [
+            httpBatchLink({
+                url: '/api/trpc'
+            })
+        ]
+    }))
 }
 
 export default Providers
