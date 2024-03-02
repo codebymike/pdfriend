e600dbc58029a1b826b51d50764a25322d55a2d3
Mike Thomas <mikethomas84@gmail.com>
2024-03-02 16:48:33 +0000

    await db user

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,13 +1,16 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server';
 import { publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
  
 export const appRouter = router({
-  authCallback: publicProcedure.query(() => {
+  authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession()
     const user = getUser()
 
     if( !user ) throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+    const dbUser = await db.user
 
     return { success: true }
   })
