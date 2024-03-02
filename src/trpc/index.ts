b0f1e11021f090acdfd5ecfd847ac224696dfca2
Mike Thomas <mikethomas84@gmail.com>
2024-03-02 16:52:53 +0000

    create user

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -6,11 +6,25 @@ import { db } from '@/db';
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession()
-    const user = getUser()
+    const user = await getUser()
 
     if( !user ) throw new TRPCError({ code: 'UNAUTHORIZED' })
 
-    const dbUser = await db.user
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id
+      }
+    })
+
+    if(!dbUser) {
+      // create user
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email ?? ""
+        }
+      })
+    }
 
     return { success: true }
   })
