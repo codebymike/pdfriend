21aa4327f05571e136c60ed57c4f2175675bab3b
Mike Thomas <mikethomas84@gmail.com>
2024-03-03 14:18:52 +0000

    controlled comp

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -1,11 +1,18 @@
 'use client'
 
+import { Dialog } from "@radix-ui/react-dialog"
 import { useState } from "react"
 
 const UploadButton = () => {
 const [isOpen, setIsOpen] = useState<boolean>(false)
 
-    return (<></>)
+    return (
+        <Dialog open={isOpen} onOpenChange={(v) => {
+            if(!v) {
+                setIsOpen(v)
+            }
+        }}></Dialog>
+    )
 }
 
 export default UploadButton
