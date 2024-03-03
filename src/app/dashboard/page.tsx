49af012fb04d22cb1c28551a5a5331a63ad891b8
Mike Thomas <mikethomas84@gmail.com>
2024-03-03 13:48:33 +0000

    Dashboard comp init

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/dist/types/server'
 import { redirect } from 'next/navigation'
 import React, { use } from 'react'
+import Dashboard from '../components/Dashboard'
 
 const page = async () => {
     const { getUser } = getKindeServerSession()
