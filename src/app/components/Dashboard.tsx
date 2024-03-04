72dd78a34f39e9cd31c4e9e66dde228234e8712b
Mike Thomas <mikethomas84@gmail.com>
2024-03-04 17:45:10 +0000

    filename styled

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -28,15 +28,16 @@ const Dashboard = () => {
                     new Date(a.createdAt).getTime()
                 ).map((file) => (
                     <li key={file.id} className='col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow transition hover:shadow-lg'>
-                        <Link href={`/dashboard/${file.id}`} className="flex flex-col gap-2">
-                            <div className="pt-6 px-6 flex w-full items-center justify-between space-x-6">
-                                <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500'>
-                                    <div className="flex-1 truncate">
-                                        <div className="flex items-center space-x-3">
-                                            <h3 className='truncate text-lg font-medium text-zinc-900'>
-                                                {file.name}
-                                            </h3>
-                                        </div>
+                        <Link
+                        href={`/dashboard/${file.id}`}
+                        className='flex flex-col gap-2'>
+                            <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
+                                <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500' />
+                                <div className='flex-1 truncate'>
+                                    <div className='flex items-center space-x-3'>
+                                        <h3 className='truncate text-lg font-medium text-zinc-900'>
+                                        {file.name}
+                                        </h3>
                                     </div>
                                 </div>
                             </div>
