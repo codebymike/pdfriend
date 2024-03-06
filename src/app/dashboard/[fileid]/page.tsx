3ac9f8509a49990b2f47a825fb21509f9ba128c6
Mike Thomas <mikethomas84@gmail.com>
2024-03-06 14:45:36 +0000

    get user

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,12 +1,20 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/types/server"
+import { redirect } from "next/navigation"
+
 interface PageProps {
     params: {
         fileid: string
     }
 }
 
-const page = ({params} : PageProps) => {
+const page =  async ({params} : PageProps) => {
 
-    const {fileid} = params
+    const { fileid } = params
+
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
+
+    if( !user || !user.id ) redirect("/auth-callback")
 
     return (
         <div>{fileid}</div>
