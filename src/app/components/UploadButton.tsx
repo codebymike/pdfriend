a28368ec0c7c037cc2bbf041aad1ea57630c1b15
Mike Thomas <mikethomas84@gmail.com>
2024-03-08 14:26:16 +0000

    dialog message

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -4,6 +4,7 @@ import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
 import { useState } from "react"
 import { Button } from "./ui/button"
 import DropZone from 'react-dropzone'
+import { Cloud } from "lucide-react"
 
 const UploadButton = () => {
 const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -20,7 +21,15 @@ const UploadDropzone = () => {
                                 htmlFor='dropzone-file'
                                 className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
                                 <div className='flex flex-col items-center justify-center pt-5 pb-6'>
-
+                                    <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                                    <p className="mb-2 text-sm text-zinc-700">
+                                        <span className="font-semibold">
+                                            Click to upload
+                                        </span>
+                                        {' '}
+                                        or drag and drop
+                                    </p>
+                                    <p>PDF (up to 4MB)</p>
                                 </div>
                             </label>
                         </div>
