f6ece222bd560573401817c1237f1a0e1e7e6fd4
Mike Thomas <mikethomas84@gmail.com>
2024-03-08 14:34:16 +0000

    accepted file markup

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -4,7 +4,7 @@ import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
 import { useState } from "react"
 import { Button } from "./ui/button"
 import DropZone from 'react-dropzone'
-import { Cloud } from "lucide-react"
+import { Cloud, File } from "lucide-react"
 
 const UploadButton = () => {
 const [isOpen, setIsOpen] = useState<boolean>(false)
@@ -16,25 +16,36 @@ const UploadDropzone = () => {
                 <div
                     {...getRootProps()}
                     className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
-                        <div className='flex items-center justify-center h-full w-full'>
-                            <label
-                                htmlFor='dropzone-file'
-                                className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
-                                <div className='flex flex-col items-center justify-center pt-5 pb-6'>
-                                    <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
-                                    <p className="mb-2 text-sm text-zinc-700">
-                                        <span className="font-semibold">
-                                            Click to upload
-                                        </span>
-                                        {' '}
-                                        or drag and drop
-                                    </p>
-                                    <p>PDF (up to 4MB)</p>
+                    <div className='flex items-center justify-center h-full w-full'>
+                        <label
+                            htmlFor='dropzone-file'
+                            className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
+                            <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+                                <Cloud className="h-6 w-6 text-zinc-500 mb-2" />
+                                <p className="mb-2 text-sm text-zinc-700">
+                                    <span className="font-semibold">
+                                        Click to upload
+                                    </span>
+                                    {' '}
+                                    or drag and drop
+                                </p>
+                                <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+                            </div>
+
+                            { acceptedFiles && acceptedFiles[0] ? (
+                                <div className="max-w-sx bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-zinc-200">
+                                    <div className="px-3 py-2 h-full grid place-items-center">
+                                        <File className="h-4 w-4 text-blue-500" />
+                                    </div>
+                                    <div className="px-3 py-2 h-full text-sm truncate">
+
+                                    </div>
                                 </div>
-                            </label>
-                        </div>
+                            ) : null }
+                        </label>
                     </div>
-            )}
+                </div>
+        )}
         </DropZone>
     )
 }
