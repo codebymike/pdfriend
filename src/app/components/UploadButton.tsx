0de840cf0f3f08ebaf63f9d51883d3a2220f205d
Mike Thomas <mikethomas84@gmail.com>
2024-03-08 14:22:52 +0000

    dialog comp working

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Dialog, DialogContent, DialogTrigger } from "@radix-ui/react-dialog"
+import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
 import { useState } from "react"
 import { Button } from "./ui/button"
 import DropZone from 'react-dropzone'
@@ -8,28 +8,43 @@ import DropZone from 'react-dropzone'
 const UploadButton = () => {
 const [isOpen, setIsOpen] = useState<boolean>(false)
 
-const UploadDropZone = () => {
-    return <DropZone multiple={false}>
-        {({ getRootProps, getInputProps, acceptedFiles }) => (
-            <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-300 rounded-lg">
-
-            </div>
-        )}
-    </DropZone>
-}
-
+const UploadDropzone = () => {
     return (
-        <Dialog open={isOpen} onOpenChange={(v) => {
-            if(!v) {
+        <DropZone multiple={false}>
+            {({ getRootProps, getInputProps, acceptedFiles }) => (
+                <div
+                    {...getRootProps()}
+                    className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
+                        <div className='flex items-center justify-center h-full w-full'>
+                            <label
+                                htmlFor='dropzone-file'
+                                className='flex flex-col items-center justify-center w-full h-full rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'>
+                                <div className='flex flex-col items-center justify-center pt-5 pb-6'>
+
+                                </div>
+                            </label>
+                        </div>
+                    </div>
+            )}
+        </DropZone>
+    )
+}
+    return (
+        <Dialog
+            open={isOpen}
+            onOpenChange={(v) => {
+                if (!v) {
                 setIsOpen(v)
-            }
-        }}>
-            <DialogTrigger onClick={() => setIsOpen(true)} asChild>
+                }
+            }}>
+            <DialogTrigger
+                onClick={() => setIsOpen(true)}
+                asChild>
                 <Button>Upload PDF</Button>
             </DialogTrigger>
 
             <DialogContent>
-                <UploadDropZone />
+                <UploadDropzone />
             </DialogContent>
         </Dialog>
     )
