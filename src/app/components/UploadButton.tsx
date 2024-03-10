5d65b824df3e5837b379e0387d644c954413f389
Mike Thomas <mikethomas84@gmail.com>
2024-03-10 14:14:11 +0000

    progress interval logic

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -13,6 +13,21 @@ const [isOpen, setIsOpen] = useState<boolean>(false)
 const UploadDropzone = () => {
 
     const [isUploading, setIsUploading] = useState<boolean|null>(true)
+    const [uploadProgress, setUploadProgress] = useState<number>(0)
+
+    const startSimulatedProgress = () => {
+        setUploadProgress(0)
+
+        const interval = setInterval(() => {
+            setUploadProgress( ( prevProgress ) => {
+                if( prevProgress >= 95 ) {
+                    clearInterval(interval)
+                    return prevProgress
+                }
+                return prevProgress + 5
+            })
+        }, 500)
+    }
 
     return (
         <DropZone multiple={false}>
