59c58eb2baec5848525be4ac34dd9e1f41bd998e
Mike Thomas <mikethomas84@gmail.com>
2024-03-10 14:56:04 +0000

    toast integ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Providers from "./components/Providers";
 
 import 'react-loading-skeleton/dist/skeleton.css'
+import { Toast } from "./components/ui/toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <Providers>
         <body 
           className={cn("min-h-screen font-sans antialiased grainy", inter.className)}>
+            <Toast />
             <Navbar />
             {children}
         </body>
