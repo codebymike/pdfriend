05544452f5fb332f5edd2366a2fe94f5d2a5b0ad
Mike Thomas <mikethomas84@gmail.com>
2024-03-10 14:43:23 +0000

    UT authorisation

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,11 +1,18 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { createUploadthing, type FileRouter } from "uploadthing/next";
  
 const f = createUploadthing();
  
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
-    .middleware(async ({ req }) => {
-      return { };
+    .middleware(async ( { req }) => {
+
+        const { getUser } = getKindeServerSession()
+        const user = await getUser()
+
+        if( !user || !user.id ) throw new Error("Unauthorised")
+
+      return { userId: user.id }
     })
     .onUploadComplete(async ({ metadata, file }) => {}),
 } satisfies FileRouter;
