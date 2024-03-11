00c78bafddc8207fe2436b485dec2a99faa044c5
Mike Thomas <mikethomas84@gmail.com>
2024-03-11 18:10:56 +0000

    allow pdfs

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -5,7 +5,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
  
 export const ourFileRouter = {
-  pdfUploader: f({ image: { maxFileSize: "4MB" } })
+  pdfUploader: f({ pdf: { maxFileSize: "4MB" } })
     .middleware(async ( { req }) => {
 
         const { getUser } = getKindeServerSession()
