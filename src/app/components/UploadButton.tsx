6f84601ec9f5ba256ae59f41eac24759f7e45cc5
Mike Thomas <mikethomas84@gmail.com>
2024-03-11 18:18:04 +0000

    redirecting note

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -4,7 +4,7 @@ import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog"
 import { useState } from "react"
 import { Button } from "./ui/button"
 import DropZone from 'react-dropzone'
-import { Cloud, File } from "lucide-react"
+import { Cloud, File, Loader2 } from "lucide-react"
 import { Progress } from "@radix-ui/react-progress"
 import { useUploadThing } from "@/lib/uploadthing"
 import { useToast } from "./ui/use-toast"
@@ -117,6 +117,12 @@ const UploadDropzone = () => {
                             { isUploading ? (
                                 <div className="w-full mt-4 max-w-xs mx-auto">
                                     <Progress value={uploadProgress} className="h-1 w-full bg-zinc-200" />
+                                    { uploadProgress === 100 ? (
+                                        <div className="flex gap-1 items-center justify-center text-sm text-zinc-700 text-center pt-2">
+                                            <Loader2 className="h-3 w-3 animate-spin" />
+                                            Redirecting...
+                                        </div>
+                                    ) : null}
                                 </div>
                             ) : null }
 
