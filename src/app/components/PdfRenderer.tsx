745b80eee13149557746a97554ba130cd5113103
Mike Thomas <mikethomas84@gmail.com>
2024-03-12 16:37:33 +0000

    pdf worker

--- src/app/components/PdfRenderer.tsx
+++ src/app/components/PdfRenderer.tsx
@@ -1,6 +1,8 @@
-import { Document, Page } from "react-pdf"
-import 'react-pdf/dist/Page/AnnotationLayer.css'
-import 'react-pdf/dist/Page/TextLayer.css'
+import { Document, Page, pdfjs } from "react-pdf"
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
+import 'react-pdf/dist/esm/Page/TextLayer.css'
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 interface PdfRenderProps {
   url: string
