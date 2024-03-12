c8f91febfb1fe91a6c2e9189ea0e7a4b81508662
Mike Thomas <mikethomas84@gmail.com>
2024-03-12 16:32:10 +0000

    url as props

--- src/app/components/PdfRenderer.tsx
+++ src/app/components/PdfRenderer.tsx
@@ -1,8 +1,12 @@
-import { Document } from "react-pdf"
+import { Document, Page } from "react-pdf"
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
 
-const PdfRenderer = () => {
+interface PdfRenderProps {
+  url: string
+}
+
+const PdfRenderer = ({ url }: PdfRenderProps ) => {
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
         <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
@@ -13,7 +17,9 @@ const PdfRenderer = () => {
 
         <div className="flex-1 w-full max-h-screen">
           <div className="">
-            <Document />
+            <Document className="max-h-full">
+              <Page pageNumber={1} />
+            </Document>
           </div>
         </div>
     </div>
