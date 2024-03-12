01a5269a47a1974fbd1e3a0ea428ae11a4b7b849
Mike Thomas <mikethomas84@gmail.com>
2024-03-12 16:41:46 +0000

    on error toast

--- src/app/components/PdfRenderer.tsx
+++ src/app/components/PdfRenderer.tsx
@@ -4,6 +4,7 @@ import { Loader2 } from "lucide-react"
 import { Document, Page, pdfjs } from "react-pdf"
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 import 'react-pdf/dist/esm/Page/TextLayer.css'
+import { useToast } from "./ui/use-toast"
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -12,6 +13,9 @@ interface PdfRenderProps {
 }
 
 const PdfRenderer = ({ url }: PdfRenderProps ) => {
+
+  const {toast} = useToast()
+
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
         <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
@@ -22,11 +26,22 @@ const PdfRenderer = ({ url }: PdfRenderProps ) => {
 
         <div className="flex-1 w-full max-h-screen">
           <div className="">
-            <Document loading={
-              <div className="flex justify-center">
-                <Loader2 className="my-24 h-6 w-6 animate-spin" />
-              </div>
-            } file={url} className="max-h-full">
+            <Document 
+              loading={
+                <div className="flex justify-center">
+                  <Loader2 className="my-24 h-6 w-6 animate-spin" />
+                </div>
+              } 
+              file={url} 
+              onLoadError={() => {
+                toast({
+                  title: "Error loading PDF",
+                  description: "Please try again later",
+                  variant: "destructive"
+                })
+              }}
+              className="max-h-full"
+              >
               <Page pageNumber={1} />
             </Document>
           </div>
