5e51c779668377bfd8b835ffa4f59e865ba4f95e
Mike Thomas <mikethomas84@gmail.com>
2024-03-15 18:01:36 +0000

    message init

--- src/app/components/ChatWrapper.tsx
+++ src/app/components/ChatWrapper.tsx
@@ -1,6 +1,12 @@
+import Message from "./Message"
+
 const ChatWrapper = () => {
   return (
-    <div>ChatWrapper</div>
+    <div className="relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2">
+      <div className="flex-1 justify-between flex flex-col mb-28">
+        <Message />
+      </div>
+    </div>
   )
 }
 
