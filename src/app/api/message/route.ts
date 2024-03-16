159f74e3b2aec2b82c0b087085ae32fd99cb2f6d
Mike Thomas <mikethomas84@gmail.com>
2024-03-16 14:33:49 +0000

    message api route

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,29 @@
+import { db } from '@/db';
+import { SendMessageValidator } from '@/lib/validators/SendMessageValidator';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextRequest } from "next/server";
+
+// asking a question of the PDF file
+export const POST = async (req: NextRequest) => {
+    const body = await req.json()
+
+    const { getUser } = getKindeServerSession()
+    const user = await getUser()
+
+    const { id: userId } = user ?? {}
+
+    if( !userId ) return new Response("Unauthorized", { status: 401 })
+
+    const { fileId, message } = SendMessageValidator.parse(body)
+
+    const file = await db.file.findFirst({
+        where: {
+            id: fileId,
+            userId
+        }
+    })
+
+    if( !file ) return new Response("Not Found", { status: 404 })
+
+    await db.message.create()
+}
