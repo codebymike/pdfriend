81f766f084946de446b69227a873eeb9f4fee32c
Mike Thomas <mikethomas84@gmail.com>
2024-03-19 17:05:07 +0000

    get message flatmap

--- src/app/components/chat/Messages.tsx
+++ src/app/components/chat/Messages.tsx
@@ -1,11 +1,31 @@
 import { trpc } from "@/app/_trpc/client"
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query"
 
-const Messages = () => {
+interface MessagesProps {
+  fileId: string
+}
 
-  const {} = trpc
+const Messages = ({fileId}: MessagesProps) => {
+
+  const { data, isLoading, fetchNextPage } = trpc.getFileMessages.useInfiniteQuery({
+    fileId,
+    limit: INFINITE_QUERY_LIMIT,
+  },{
+    getNextPageParam: (lastPage) => lastPage?.nextCursor,
+    // keepPreviousData: true
+  })
+
+  const messages = data?.pages.flatMap((page) => page.messages)
+
+  const combinedMessages = [
+    ...(true ? [loadingMessage] : []),
+    ...
+  ]
 
   return (
-    <div></div>
+    <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrollbar-touch">
+      {  }
+    </div>
   )
 }
 
