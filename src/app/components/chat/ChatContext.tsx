3559230ae3e378097f89ca30b7541ec2d288cb62
Mike Thomas <mikethomas84@gmail.com>
2024-03-20 13:06:25 +0000

    add chucks to messages

--- src/app/components/chat/ChatContext.tsx
+++ src/app/components/chat/ChatContext.tsx
@@ -128,7 +128,60 @@ export const ChatContextProvider = ({ fileId, children }: Props) => {
             accResponse += chunkValue
 
             // add chunk to message
-            
+            utils.getFileMessages.setInfiniteData(
+                { fileId, limit: INFINITE_QUERY_LIMIT },
+                (old) => {
+                    if (!old) return { pages: [], pageParams: [] }
+
+                    let isAiResponseCreated = old.pages.some(
+                      (page) =>
+                        page.messages.some(
+                          (message) => message.id === 'ai-response'
+                        )
+                    )
+
+                    let updatedPages = old.pages.map((page) => {
+                        if( page === old.pages[0] ) {
+                            let updatedMessages
+
+                            if( !isAiResponseCreated ) {
+                                updatedMessages = [
+                                    {
+                                        createdAt: new Date().toISOString(),
+                                        id: "ai-response",
+                                        text: accResponse,
+                                        isUserMessage: false
+                                    },
+                                    ...page.messages
+                                ]
+                            } else {
+                                updatedMessages = page.messages.map( message => {
+                                    if( message.id == "ai-response" ) {
+                                        return {
+                                            ...message,
+                                            text: accResponse
+                                        }
+                                    }
+                                    return message
+                                })
+                            }
+
+                            return {
+                                ...page,
+                                messages: updatedMessages
+                            }
+                        }
+
+                        return page
+                    })
+
+                    return {
+                        ...old,
+                        pages: updatedPages
+                    }
+                }
+
+            )
         }
 
     },
