cb9a11f0590f786a3489a695c755d996a9d6d2ff
Mike Thomas <mikethomas84@gmail.com>
2024-03-21 15:14:56 +0000

    price ids

---
+++ src/config/stripe.ts
@@ -0,0 +1,28 @@
+export const PLANS = [
+    {
+        name: "Free",
+        slug: "free",
+        quota: 1,
+        pagesPerPdf: 2,
+        price: {
+            amount: 0,
+            priceIds: {
+                test: '',
+                production: ''
+            }
+        }
+    },
+    {
+        name: "Pro",
+        slug: "pro",
+        quota: 50,
+        pagesPerPdf: 25,
+        price: {
+            amount: 10,
+            priceIds: {
+                test: 'price_1OwnGYJdw5ibQ7q5wHMleUSq',
+                production: 'price_1OwnGYJdw5ibQ7q5wHMleUSq'
+            }
+        }
+    }
+]
