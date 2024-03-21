daefc249cf8ef1c974e7b67ebc5de4cc180d1e06
Mike Thomas <mikethomas84@gmail.com>
2024-03-21 14:56:55 +0000

    chat context missing prop

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -41,7 +41,7 @@ const page =  async ({params} : PageProps) => {
 
                 {/* right side */}
                 <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-                    <ChatWrapper fileId={file.id} />
+                    <ChatWrapper fileId={file.id} isSubscribed={true} />
                 </div>
 
             </div>
