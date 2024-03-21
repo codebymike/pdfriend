35437ce500412c4ac1a7419bec2f014d321672fe
Mike Thomas <mikethomas84@gmail.com>
2024-03-21 15:35:34 +0000

    pricing content blocks

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,7 +1,9 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import MaxWidthWrapper from "../components/MaxWidthWrapper"
-import { TooltipProvider } from "../components/ui/tooltip"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../components/ui/tooltip"
 import { PLANS } from "@/config/stripe"
+import { cn } from "@/lib/utils"
+import { HelpCircle } from "lucide-react"
 
 const Page = async () => {
     const { getUser } = getKindeServerSession()
@@ -77,7 +79,62 @@ const Page = async () => {
 
                 <div className='pt-12 grid grid-cols-1 gap-10 lg:grid-cols-2'>
                     <TooltipProvider>
-                        <div></div>
+                        { pricingItems.map( ({plan, tagline, quota, features }) => {
+                            const price = PLANS.find( p  => p.slug === plan.toLowerCase())?.price.amount || 0
+                            return (
+                                <div key={plan} className={cn(
+                                    'relative rounded-2xl bg-white shadow-lg',
+                                    {
+                                      'border-2 border-blue-600 shadow-blue-200':
+                                        plan === 'Pro',
+                                      'border border-gray-200':
+                                        plan !== 'Pro',
+                                    }
+                                  )}>
+                                    { plan === "Pro" && (
+                                        <div className='absolute -top-5 left-0 right-0 mx-auto w-32 rounded-full bg-gradient-to-r from-blue-600 to-cyan-600 px-3 py-2 text-sm font-medium text-white'>
+                                            Upgrade now
+                                        </div>
+                                    )}
+
+                                    <div className='p-5'>
+                                        <h3 className='my-3 text-center font-display text-3xl font-bold'>
+                                            {plan}
+                                        </h3>
+                                        <p className='text-gray-500'>
+                                            {tagline}
+                                        </p>
+                                        <p className='my-5 font-display text-6xl font-semibold'>
+                                            ${price}
+                                        </p>
+                                        <p className='text-gray-500'>
+                                            per month
+                                        </p>
+                                    </div>
+
+                                    <div className='flex h-20 items-center justify-center border-b border-t border-gray-200 bg-gray-50'>
+                                        <div className='flex items-center space-x-1'>
+                                            <p>
+                                            {quota.toLocaleString()} PDFs/mo
+                                            included
+                                            </p>
+
+                                            <Tooltip delayDuration={300}>
+                                            <TooltipTrigger className='cursor-default ml-1.5'>
+                                                <HelpCircle className='h-4 w-4 text-zinc-500' />
+                                            </TooltipTrigger>
+                                            <TooltipContent className='w-80 p-2'>
+                                                How many PDFs you can upload per
+                                                month.
+                                            </TooltipContent>
+                                            </Tooltip>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            )
+                            
+                        })}
                     </TooltipProvider>
                 </div>
 
