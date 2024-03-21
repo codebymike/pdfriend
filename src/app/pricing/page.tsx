6843bd4741cb993b2626eead34f95825b02f16b8
Mike Thomas <mikethomas84@gmail.com>
2024-03-21 15:24:39 +0000

    map over pricing models

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,10 +1,66 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import MaxWidthWrapper from "../components/MaxWidthWrapper"
 import { TooltipProvider } from "../components/ui/tooltip"
+import { PLANS } from "@/config/stripe"
 
 const Page = async () => {
     const { getUser } = getKindeServerSession()
     const user = await getUser()
+
+    const pricingItems = [
+        {
+          plan: 'Free',
+          tagline: 'For small side projects.',
+          quota: 10,
+          features: [
+            {
+              text: '5 pages per PDF',
+              footnote: 'The maximum amount of pages per PDF-file.',
+            },
+            {
+              text: '4MB file size limit',
+              footnote: 'The maximum file size of a single PDF file.',
+            },
+            {
+              text: 'Mobile-friendly interface',
+            },
+            {
+              text: 'Higher-quality responses',
+              footnote: 'Better algorithmic responses for enhanced content quality',
+              negative: true,
+            },
+            {
+              text: 'Priority support',
+              negative: true,
+            },
+          ],
+        },
+        {
+          plan: 'Pro',
+          tagline: 'For larger projects with higher needs.',
+          quota: PLANS.find((p) => p.slug === 'pro')!.quota,
+          features: [
+            {
+              text: '25 pages per PDF',
+              footnote: 'The maximum amount of pages per PDF-file.',
+            },
+            {
+              text: '16MB file size limit',
+              footnote: 'The maximum file size of a single PDF file.',
+            },
+            {
+              text: 'Mobile-friendly interface',
+            },
+            {
+              text: 'Higher-quality responses',
+              footnote: 'Better algorithmic responses for enhanced content quality',
+            },
+            {
+              text: 'Priority support',
+            },
+          ],
+        },
+      ]
 
     return (
         <>
@@ -21,7 +77,7 @@ const Page = async () => {
 
                 <div className='pt-12 grid grid-cols-1 gap-10 lg:grid-cols-2'>
                     <TooltipProvider>
-                        
+                        <div></div>
                     </TooltipProvider>
                 </div>
 
