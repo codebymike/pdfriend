8899f214431d224a3bf72a2588246c819052d09a
Mike Thomas <mikethomas84@gmail.com>
2024-03-23 23:30:39 +0000

    billing form props

--- src/app/components/BillingForm.tsx
+++ src/app/components/BillingForm.tsx
@@ -1,4 +1,14 @@
-const BillingForm = () => {
+"use client"
+
+import { getUserSubscriptionPlan } from "@/lib/stripe"
+
+interface BillingFormProps {
+    subscriptionPlan: Awaited<
+        ReturnType<typeof getUserSubscriptionPlan>
+    >
+}
+
+const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
   return (
     <div>BillingForm</div>
   )
