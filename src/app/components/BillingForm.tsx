5b237fc7ce6370eeaaf52410a42e6151f94cd7dd
Mike Thomas <mikethomas84@gmail.com>
2024-03-23 23:46:04 +0000

    billing form complete

--- src/app/components/BillingForm.tsx
+++ src/app/components/BillingForm.tsx
@@ -7,6 +7,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
 import { Button } from "./ui/button"
 import { Loader2 } from "lucide-react"
+import { format } from "date-fns"
 
 interface BillingFormProps {
     subscriptionPlan: Awaited<
@@ -17,7 +18,7 @@ interface BillingFormProps {
 const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
     const { toast } = useToast()
 
-    const { mutate: createStripeSession, isLoading } = trpc.createStripeSession.useMutation({
+    const { mutate: createStripeSession, isPending } = trpc.createStripeSession.useMutation({
         onSuccess: ({ url }) => {
             if( url ) window.location.href = url
             else {
@@ -48,13 +49,23 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
 
                 <CardFooter className='flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0'>
                     <Button type='submit'>
-                    {isLoading ? (
+                    {isPending ? (
                         <Loader2 className='mr-4 h-4 w-4 animate-spin' />
                     ) : null}
                     {subscriptionPlan.isSubscribed
                         ? 'Manage Subscription'
                         : 'Upgrade to PRO'}
                     </Button>
+
+                    {subscriptionPlan.isSubscribed ? (
+                    <p className='rounded-full text-xs font-medium'>
+                        {subscriptionPlan.isCanceled
+                        ? 'Your plan will be canceled on '
+                        : 'Your plan renews on'}
+                        {format( subscriptionPlan.stripeCurrentPeriodEnd!, 'dd.MM.yyyy')}
+                        .
+                    </p>
+                    ) : null}
                 </CardFooter>
             </Card>
         </form>
