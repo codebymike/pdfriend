73e78ec8a3901b26a57796d4b0311cba64ef681f
Mike Thomas <mikethomas84@gmail.com>
2024-03-23 23:38:53 +0000

    billing form skeleton

--- src/app/components/BillingForm.tsx
+++ src/app/components/BillingForm.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import { getUserSubscriptionPlan } from "@/lib/stripe"
+import { useToast } from "./ui/use-toast"
+import { trpc } from "@/app/_trpc/client"
+import MaxWidthWrapper from "./MaxWidthWrapper"
 
 interface BillingFormProps {
     subscriptionPlan: Awaited<
@@ -9,6 +12,33 @@ interface BillingFormProps {
 }
 
 const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
+    const { toast } = useToast()
+
+    const { mutate: createStripeSession, isLoading } = trpc.createStripeSession.useMutation({
+        onSuccess: ({ url }) => {
+            if( url ) window.location.href = url
+            else {
+                toast({
+                    title: "There was a problem with Stripe",
+                    description: "Please try again in a moment",
+                    variant: "destructive"
+                })
+            }
+        }
+    })
+
+    return <MaxWidthWrapper className="max-w-5xl">
+        <form 
+            className="mt-12"
+            onSubmit={(e) => {
+            e.preventDefault()
+            createStripeSession()
+        }}>
+
+        </form>
+    </MaxWidthWrapper>
+
+
   return (
     <div>BillingForm</div>
   )
