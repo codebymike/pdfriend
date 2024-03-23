627dc2f6f03d8cf29dc8516ca0d2ef91a335929d
Mike Thomas <mikethomas84@gmail.com>
2024-03-23 23:42:06 +0000

    billing card

--- src/app/components/BillingForm.tsx
+++ src/app/components/BillingForm.tsx
@@ -4,6 +4,9 @@ import { getUserSubscriptionPlan } from "@/lib/stripe"
 import { useToast } from "./ui/use-toast"
 import { trpc } from "@/app/_trpc/client"
 import MaxWidthWrapper from "./MaxWidthWrapper"
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card"
+import { Button } from "./ui/button"
+import { Loader2 } from "lucide-react"
 
 interface BillingFormProps {
     subscriptionPlan: Awaited<
@@ -34,7 +37,26 @@ const BillingForm = ({ subscriptionPlan }: BillingFormProps) => {
             e.preventDefault()
             createStripeSession()
         }}>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Subscription Plan</CardTitle>
+                    <CardDescription>
+                        You are currently on the{' '}
+                        <strong>{subscriptionPlan.name}</strong> plan.
+                    </CardDescription>
+                </CardHeader>
 
+                <CardFooter className='flex flex-col items-start space-y-2 md:flex-row md:justify-between md:space-x-0'>
+                    <Button type='submit'>
+                    {isLoading ? (
+                        <Loader2 className='mr-4 h-4 w-4 animate-spin' />
+                    ) : null}
+                    {subscriptionPlan.isSubscribed
+                        ? 'Manage Subscription'
+                        : 'Upgrade to PRO'}
+                    </Button>
+                </CardFooter>
+            </Card>
         </form>
     </MaxWidthWrapper>
 
